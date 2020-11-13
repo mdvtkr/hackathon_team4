433d5c90f0d0f3c5186db9110f2195089e739ff4
heover1cks-private <heover1cks@gmail.com>
2020-11-13 16:58:18 +0900

    DB Models Added

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,73 @@
 from django.contrib import admin
+from . import models
+
+@admin.register(models.currentStockPrice)
+class CurrentStockPrice(admin.ModelAdmin):
+    list_display = (
+        'stock_code',
+        'stock_name',
+        'current_price'
+    )
+
+@admin.register(models.newsData)
+class NewsData(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'news_data',
+        'stock_name',
+        'current_price',
+        'rate_005930',
+        'rate_000660',
+        'rate_068270',
+        'rate_096530',
+        'rate_105560',
+        'rate_055550',
+        'rate_009540',
+        'rate_133750',
+        'rate_057030',
+        'rate_035720',
+        'rate_035420',
+        'rate_035760',
+        'rate_352820',
+        'rate_009830',
+        'rate_011780',
+        'rate_011070'
+    )
+
+
+@admin.register(models.currentStock)
+class CurrentStock(admin.ModelAdmin):
+    list_display = (
+        'stock_code',
+        'stock_quantity',
+        'average_price',
+        'current_price',
+        'earning_rate'
+    )
+
+@admin.register(models.userRank)
+class UserRank(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'username',
+        'earning_rate'
+    )
+
+@admin.register(models.tradingLog)
+class TradingLog(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'round',
+        'stock_code',
+        'sell_count',
+        'buy_count'
+    )
+
+@admin.register(models.stockReference)
+class StockReference(admin.ModelAdmin):
+    list_display = (
+        'stock_code',
+        'stock_name'
+    )
 
 # Register your models here.
--- web/models.py
+++ web/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
+class currentStockPrice(models.Model):
+    stock_code = models.IntegerField(primary_key=True)
+    stock_name = models.CharField(max_length=30)
+    current_price = models.IntegerField()
+
+class newsData(models.Model):
+    id = models.AutoField(primary_key=True)
+    news_data = models.CharField(max_length=300)
+    stock_name = models.CharField(max_length=30)
+    current_price = models.IntegerField()
+    rate_005930 = models.IntegerField()
+    rate_000660 = models.IntegerField()
+    rate_068270 = models.IntegerField()
+    rate_096530 = models.IntegerField()
+    rate_105560 = models.IntegerField()
+    rate_055550 = models.IntegerField()
+    rate_009540 = models.IntegerField()
+    rate_133750 = models.IntegerField()
+    rate_057030 = models.IntegerField()
+    rate_035720 = models.IntegerField()
+    rate_035420 = models.IntegerField()
+    rate_035760 = models.IntegerField()
+    rate_352820 = models.IntegerField()
+    rate_009830 = models.IntegerField()
+    rate_011780 = models.IntegerField()
+    rate_011070 = models.IntegerField()
+
+class currentStock(models.Model):
+    stock_code = models.IntegerField(primary_key=True)
+    stock_quantity = models.IntegerField()
+    average_price = models.IntegerField()
+    current_price = models.IntegerField()
+    earning_rate = models.IntegerField()
+
+class userRank(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length = 30)
+    earning_rate = models.IntegerField()
+    # date = models.DateTimeField(auto_now_add=True)
+
+class tradingLog(models.Model):
+    id = models.AutoField(primary_key=True)
+    round = models.IntegerField()
+    stock_code = models.IntegerField()
+    sell_count = models.IntegerField()
+    buy_count = models.IntegerField()
+
+class stockReference(models.Model):
+    stock_code = models.IntegerField(primary_key=True)
+    stock_name = models.CharField(max_length = 30)
+
 # Create your models here.
