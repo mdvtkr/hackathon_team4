9c230e2d3e2652f85e073ea47d703ee83764c9ea
heover1cks-private <heover1cks@gmail.com>
2020-11-14 20:18:33 +0900

    Stock Price Open API Requests

--- web/urls.py
+++ web/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('intro/', views.intro, name='intro'),
     path('test/', views.test, name='test'),
     path('base/', views.base, name='base'),
+    path('api/stock_price', views.getCurrentStockPrice, name='api')
 ]
--- web/views.py
+++ web/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.views import View
 from django.conf import settings
 
+from django.http import JsonResponse
+import urllib.request as req
+import urllib.parse as parse
+import requests
+
 # Create your views here.
 def intro(request):
     return render(request, 'web/intro.html')
@@ -11,3 +16,14 @@ def test(request):
 
 def base(request):
     return render(request, 'web/base.html')
+
+def getCurrentStockPrice(request):
+    return JsonResponse(koscomApi())
+
+
+def koscomApi():
+    marketCode = 'kospi'
+    url = 'https://sandbox-apigw.koscom.co.kr/v2/market/stocks/'+marketCode+'/lists'
+    headers = {'apiKey':'l7xxc59a3df427af489fa4234dce296492f3'}
+    res = requests.get(url, headers=headers)
+    return res.json()
